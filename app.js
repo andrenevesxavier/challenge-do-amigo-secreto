d36afcba0a23f5acbccfb7093b6f976acfbfefab
André Neves Xavier <andrenevesxavier276@gmail.com>
2025-08-15 22:40:38 -0300

    substitui a função  mostrarListaNaTela, adiciona o sorteio do amigo.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 let listaDeAmigos = [];
+let ul = document.getElementById('listaAmigos');
 
 function nomeDoAmigo() {
-    let nome = document.querySelector('input').value
+    let nome = document.querySelector('input').value.trim();
         if (nome == ""){
             mostrarMensagem('h2', 'Por favor, Digite um nome válido!')
         }
         else{
             listaDeAmigos.push(nome);
             mostrarMensagem ('h2', 'Digite o nome dos seus amigos')
-        }
         limparCampo()
+        listaAmigos()   
+        }
+        
         
 console.log  (listaDeAmigos);
 
@@ -25,12 +28,32 @@ function limparCampo() {
     
 }
 
+//
 function adicionarAmigo() {
-    nomeDoAmigo()
+    nomeDoAmigo();
 }
 
-function mostrarListaNaTela(){
-    let lista = document.getElementById('listaAmigos');
-    let novoItemLista = listaDeAmigos
-    lista.appendChild(novoItemLista);
+function listaAmigos() {
+    ul
+    ul.innerHTML = '';
+    for (let i = 0; i < listaDeAmigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = (listaDeAmigos[i]);
+        ul.appendChild(li);
+        console.log (li)
+    }  
+}
+
+function sortearAmigo () {
+    if (listaDeAmigos == ""){
+        mostrarMensagem ('h2', 'Adicione seus amigos antes de sortear');
+    }
+    else{
+        let posição = parseInt(Math.random() * listaDeAmigos.length);
+        let amigoSorteado = listaDeAmigos[posição];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = (`O amigo secreto sorteado é :  ${amigoSorteado}`);
+        ul.innerHTML = ''
+    }
+    
 }
